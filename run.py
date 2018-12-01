41326ee530eaca8b7507f3cec17faa0dafdfef5f
Aris Androulidakis <lildogzsixpack@gmail.com>
2018-12-01 12:05:09 +0200

    change pytube to youtube-dl

--- run.py
+++ run.py
@@ -1,9 +1,26 @@
+from __future__ import unicode_literals
 import os
-from pytube import YouTube
+import youtube_dl
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('FLASK_KEY')
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
 
 
 @app.route('/', methods=['GET'])
@@ -14,15 +31,22 @@ def index():
 @app.route('/download', methods=['POST', 'GET'])
 def download():
     if request.method == 'POST':
-        url = str(request.form["url"])
-        yt = YouTube(url)
-        print(yt.title)
-        stream = yt.streams.filter(only_audio=True).first()
-        stream.download('/home/lildogzsixpack')
-        print("Has been successfully downloaded")
-        return render_template('index.html')
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': '/home/lildogzsixpack/%(title)s-%(id)s.%(ext)s',
+            'logger': MyLogger(),
+            'progress_hooks': [my_hook],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([request.form["url"]])
     else:
         return render_template('index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
